255bc14f134427cc5692d01c9381f105c4aef009
Julie <22972550+Cobular@users.noreply.github.com>
2024-05-16 20:12:12 -0700

    Try interrupt

--- freq_gen/src/main.rs
+++ freq_gen/src/main.rs
@@ -10,15 +10,19 @@
 #![no_std]
 #![no_main]
 
-use core::cell::Cell;
-
-use critical_section::Mutex;
-use rp_pico::hal::clocks::ClocksManager;
-use rp_pico::hal::pac::interrupt;
 use cortex_m::singleton;
+use critical_section::Mutex;
 use embedded_hal::digital::OutputPin;
+use hal::dma;
+use hal::dma::Channel;
+use hal::dma::Channels;
+use hal::dma::SingleChannel;
+
+use hal::dma::CH0;
 // The macro for our start-up function
 use rp_pico::entry;
+use rp_pico::hal::pac::interrupt;
+
 
 // GPIO traits
 use embedded_hal::pwm::SetDutyCycle;
@@ -26,8 +30,6 @@ use embedded_hal::pwm::SetDutyCycle;
 // Ensure we halt the program on panic (if we don't mention this crate it won't
 // be linked)
 use panic_halt as _;
-
-use usb_device::{class_prelude::*, prelude::*};
 
 use rp_pico::hal::adc::AdcPin;
 use rp_pico::hal::dma::single_buffer;
@@ -42,31 +44,37 @@ use rp_pico::hal::pac;
 // A shorter alias for the Hardware Abstraction Layer, which provides
 // higher-level drivers.
 use rp_pico::hal;
-use usbd_serial::SerialPort;
+use rp_pico::hal::uart::DataBits;
+use rp_pico::hal::uart::StopBits;
+use rp_pico::hal::uart::UartConfig;
+use fugit::RateExtU32;
+use core::cell::RefCell;
+use core::fmt::Write;
+use core::ops::Deref;
+
 
 const DUTY_CYCLE_PERCENT: u16 = 30;
 
-/// The USB Device Driver (shared with the interrupt).
-static mut USB_DEVICE: Option<UsbDevice<hal::usb::UsbBus>> = None;
+type TransferData<'a> = single_buffer::Transfer<Channel<CH0>, hal::adc::DmaReadTarget<u16>, &'a mut [u16; 128]>;
 
-/// The USB Bus Driver (shared with the interrupt).
-static mut USB_BUS: Option<UsbBusAllocator<hal::usb::UsbBus>> = None;
+type UartPerip = hal::uart::UartPeripheral<hal::uart::Enabled, pac::UART0, (hal::gpio::Pin<hal::gpio::bank0::Gpio0, hal::gpio::FunctionUart, hal::gpio::PullDown>, hal::gpio::Pin<hal::gpio::bank0::Gpio1, hal::gpio::FunctionUart, hal::gpio::PullDown>)>;
 
-/// The USB Serial Device Driver (shared with the interrupt).
-static mut USB_SERIAL: Option<SerialPort<hal::usb::UsbBus>> = None;
+/// Since we're always accessing these pins together we'll store them in a tuple.
+/// Giving this tuple a type alias means we won't need to use () when putting them
+/// inside an Option. That will be easier to read.
+type DmaData = (TransferData<'static>, UartPerip);
 
-static MESSAGE_QUEUE: Mutex<Cell<Option<([u8; 64], usize)>>> = Mutex::new(Cell::new(None));
+/// This how we transfer our LED pin, input pin and PWM slice into the Interrupt Handler.
+/// We'll have the option hold both using the LedAndInput type.
+/// This will make it a bit easier to unpack them later.
+static GLOBAL_PINS: Mutex<RefCell<Option<DmaData>>> = Mutex::new(RefCell::new(None));
 
-fn write(msg: &str) {
-    let mut buf = [0u8; 64];
-    let len = msg.as_bytes().len();
-    buf.iter_mut().zip(msg.as_bytes()).for_each(|(b, c)| {
-        *b = *c;
-    });
-    critical_section::with(|cs| {
-        MESSAGE_QUEUE.borrow(cs).set(Some((buf, len)));
-    });
+
+fn average_samples(samples: &[u16; 128]) -> u32 {
+    let sum: u32 = samples.iter().map(|&x| x as u32).sum();
+    sum >> 7
 }
+
 
 /// Entry point to our bare-metal application.
 ///
@@ -99,10 +107,6 @@ fn main() -> ! {
     .ok()
     .unwrap();
 
-    let timer = hal::Timer::new(pac.TIMER, &mut pac.RESETS, &clocks);
-
-    let ClocksManager { usb_clock, system_clock, peripheral_clock, .. } = clocks;
-
     // The single-cycle I/O block controls our GPIO pins
     let sio = hal::Sio::new(pac.SIO);
 
@@ -114,57 +118,28 @@ fn main() -> ! {
         &mut pac.RESETS,
     );
 
-    // Set up the USB driver
-    let usb_bus = UsbBusAllocator::new(hal::usb::UsbBus::new(
-        pac.USBCTRL_REGS,
-        pac.USBCTRL_DPRAM,
-        usb_clock,
-        true,
-        &mut pac.RESETS,
-    ));
-    unsafe {
-        // Note (safety): This is safe as interrupts haven't been started yet
-        USB_BUS = Some(usb_bus);
-    }
+    // UART TX (characters sent from pico) on pin 1 (GPIO0) and RX (on pin 2 (GPIO1)
+    let uart_pins = (
+        pins.gpio0.into_function::<hal::gpio::FunctionUart>(),
+        pins.gpio1.into_function::<hal::gpio::FunctionUart>(),
+    );
 
-    // Grab a reference to the USB Bus allocator. We are promising to the
-    // compiler not to take mutable access to this global variable whilst this
-    // reference exists!
-    let bus_ref = unsafe { USB_BUS.as_ref().unwrap() };
+    // Create a UART driver
+    let uart: UartPerip = hal::uart::UartPeripheral::new(pac.UART0, uart_pins, &mut pac.RESETS)
+        .enable(
+            UartConfig::new(115200.Hz(), DataBits::Eight, None, StopBits::One),
+            clocks.peripheral_clock.freq(),
+        )
+        .unwrap();
 
-    // Set up the USB Communications Class Device driver
-    let serial = SerialPort::new(bus_ref);
-    unsafe {
-        USB_SERIAL = Some(serial);
-    }
-
-    // Create a USB device with a fake VID and PID
-    let usb_dev = UsbDeviceBuilder::new(bus_ref, UsbVidPid(0x16c0, 0x27dd))
-        .strings(&[StringDescriptors::default()
-            .manufacturer("Fake company")
-            .product("Serial port")
-            .serial_number("TEST")])
-        .unwrap()
-        .device_class(2) // from: https://www.usb.org/defined-class-codes
-        .build();
-    unsafe {
-        // Note (safety): This is safe as interrupts haven't been started yet
-        USB_DEVICE = Some(usb_dev);
-    }
-
-    let nvic = &mut pac.i;
-
-    // Enable the USB interrupt
-    unsafe {
-        pac::NVIC::unmask(hal::pac::Interrupt::USBCTRL_IRQ);
-    };
+    uart.write_full_blocking(b"ADC FIFO DMA example\r\n");
 
     let mut led_pin = pins.led.into_push_pull_output();
     led_pin.set_high().unwrap();
 
     // The delay object lets us wait for specified amounts of time (in
     // milliseconds)
-    let mut delay = cortex_m::delay::Delay::new(core.SYST, system_clock.freq().to_Hz());
+    let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
 
     // Init PWMs
     let mut pwm_slices = hal::pwm::Slices::new(pac.PWM, &mut pac.RESETS);
@@ -193,7 +168,7 @@ fn main() -> ! {
     let _ = channel.set_duty_cycle(26);
 
     // Initialize DMA
-    let dma = pac.DMA.split(&mut pac.RESETS);
+    let mut dma = pac.DMA.split(&mut pac.RESETS);
 
     // ADC Setup
     let mut adc = hal::Adc::new(pac.ADC, &mut pac.RESETS);
@@ -203,7 +178,9 @@ fn main() -> ! {
 
     // we'll capture 1000 samples in total (500 per channel)
     // NOTE: when calling `shift_8bit` below, the type here must be changed from `u16` to `u8`
-    let buf_for_samples = singleton!(: [u16; 1000] = [0; 1000]).unwrap();
+    let buf_for_samples = singleton!(: [u16; 128] = [0; 128]).unwrap();
+
+    // To average, we sum all samples into a u32 then shift right by 7 (divide by 128)
 
     // Configure free-running mode:
     let mut adc_fifo = adc
@@ -211,7 +188,7 @@ fn main() -> ! {
         // Set clock divider to target a sample rate of 96,000 samples per second (1ksps).
         // The value was calculated by `(48MHz / 96ksps) - 1 = 499.0`
         // Please check the `clock_divider` method documentation for details.
-        .clock_divider(499, 0)
+        .clock_divider(0, 0)
         // sample the temperature sensor first
         .set_channel(&mut current_sensor_pin)
         // Uncomment this line to produce 8-bit samples, instead of 12 bit (lower bits are discarded)
@@ -221,21 +198,33 @@ fn main() -> ! {
         // Create the FIFO, but don't start it just yet
         .start_paused();
 
+    dma.ch0.enable_irq0();
+
     // Start a DMA transfer (must happen before resuming the ADC FIFO)
-    let dma_transfer =
+    let dma_transfer: TransferData =
         single_buffer::Config::new(dma.ch0, adc_fifo.dma_read_target(), buf_for_samples).start();
 
     // Resume the FIFO to start capturing
     adc_fifo.resume();
 
-    let start = timer.get_counter();
+    // Give away our pins by moving them into the `GLOBAL_PINS` variable.
+    // We won't need to access them in the main thread again
+    critical_section::with(|cs| {
+        GLOBAL_PINS.borrow(cs).replace(Some((dma_transfer, uart)));
+    });
+    adc_fifo.resume();
 
+    // Give away our pins by moving them into the `GLOBAL_PINS` variable.
+    // We won't need to access them in the main thread again
+    critical_section::with(|cs| {
+        GLOBAL_PINS.borrow(cs).replace(Some((dma_transfer, uart)));
+    });
 
-    let (_ch, _adc_read_target, buf_for_samples) = dma_transfer.wait();
+    // let timer = hal::Timer::new(pac.TIMER, &mut pac.RESETS, &clocks);
 
-    let time_taken = timer.get_counter() - start;
+    // let time_taken = timer.get_counter();
 
-    // serial.write(b"Done sampling, printing results:\r\n");
+    // uart.write_full_blocking(b"Done sampling, printing results:\r\n");
 
     // Infinite loop, fading LED up and down
     loop {
@@ -243,61 +232,57 @@ fn main() -> ! {
         delay.delay_ms(500);
         led_pin.set_low().unwrap();
         delay.delay_ms(500);
-        write("Hello, World!\r\n");
+
+        if dma_transfer.is_done() {
+            let dma_data = GLOBAL_PINS.borrow(cs).take();
+
+            if let Some((dma_transfer, mut uart)) = dma_data {
+                let (ch, adc_read_target, buf_for_samples) = dma_transfer.wait();
+        
+                // Average the samples
+                let average = average_samples(buf_for_samples);
+        
+                // Print the average
+                writeln!(uart, "Average: {}\r", average).unwrap();
+        
+                let new_dma_transfer =
+                    single_buffer::Config::new(ch, adc_read_target, buf_for_samples).start();
+        
+                GLOBAL_PINS.borrow(cs).replace(Some((new_dma_transfer, uart)));
+            }
+        }
+        // writeln!(
+        //     uart,
+        //     "Took {}ms, average: {}\r",
+        //     time_taken,
+        //     average_samples(buf_for_samples)
+        // )
+        // .unwrap();
     }
 }
 
-// End of file
 
-
-/// This function is called whenever the USB Hardware generates an Interrupt
-/// Request.
-///
-/// We do all our USB work under interrupt, so the main thread can continue on
-/// knowing nothing about USB.
+// Interrupt handler for the DMA controller
 #[allow(non_snake_case)]
 #[interrupt]
-unsafe fn USBCTRL_IRQ() {
-    // Grab the global objects. This is OK as we only access them under interrupt.
-    let usb_dev = USB_DEVICE.as_mut().unwrap();
-    let serial = USB_SERIAL.as_mut().unwrap();
-
+fn DMA_IRQ_0() {
     critical_section::with(|cs| {
-        if let Some((msg, len)) = MESSAGE_QUEUE.borrow(cs).take() {
-            let mut wr_ptr = &msg[..len];
-            while !wr_ptr.is_empty() {
-                let _ = serial.write(wr_ptr).map(|len| {
-                    wr_ptr = &wr_ptr[len..];
-                });
-            }
+        let dma_data = GLOBAL_PINS.borrow(cs).take();
+
+        if let Some((dma_transfer, mut uart, ..)) = dma_data {
+            let (ch, adc_read_target, buf_for_samples) = dma_transfer.wait();
+    
+            // Average the samples
+            let average = average_samples(buf_for_samples);
+
+            // Print the average
+            writeln!(uart, "Average: {}\r", average).unwrap();
+
+            let new_dma_transfer =
+                single_buffer::Config::new(ch, adc_read_target, buf_for_samples).start();
+
+            GLOBAL_PINS.borrow(cs).replace(Some((new_dma_transfer, uart)));
         }
     });
-    serial.flush().unwrap();
-
-    // Poll the USB driver with all of our supported USB Classes
-    if usb_dev.poll(&mut [serial]) {
-        let mut buf = [0u8; 64];
-        match serial.read(&mut buf) {
-            Err(_e) => {
-                // Do nothing
-            }
-            Ok(0) => {
-                // Do nothing
-            }
-            Ok(count) => {
-                // Convert to upper case
-                buf.iter_mut().take(count).for_each(|b| {
-                    b.make_ascii_uppercase();
-                });
-
-                // Send back to the host
-                let mut wr_ptr = &buf[..count];
-                while !wr_ptr.is_empty() {
-                    let _ = serial.write(wr_ptr).map(|len| {
-                        wr_ptr = &wr_ptr[len..];
-                    });
-                }
-            }
-        }
-    }
 }
+
